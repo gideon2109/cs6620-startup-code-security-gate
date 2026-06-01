94149c68eebed2a20906a5c067aff75f6e4e8bde
gideon2109 <ntimgyakari.g@northeastern.edu>
2026-06-01 03:48:22 +0000

    refactor: remove API Gateway module (replaced by Lambda Function URL)

--- terraform/modules/api_gateway/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_endpoint" {
-  description = "The base URL of the API Gateway"
-  value       = aws_apigatewayv2_api.this.api_endpoint
-}
-
-output "scan_url" {
-  description = "The full URL for the /scan endpoint"
-  value       = "${aws_apigatewayv2_api.this.api_endpoint}/scan"
-}
